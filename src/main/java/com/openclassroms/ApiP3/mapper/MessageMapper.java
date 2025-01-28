cee1dfbd328dbb9402816255b158931622aaae9d
chambrin <achambrin@outlook.fr>
2025-01-28 11:04:27 +0100

    ✨ Messagers mapper fix

--- src/main/java/com/openclassroms/ApiP3/mapper/MessageMapper.java
+++ src/main/java/com/openclassroms/ApiP3/mapper/MessageMapper.java
@@ -19,32 +19,35 @@ public class MessageMapper {
   @Autowired
   private RentalRepository rentalRepository;
 
-  // Convertir un MessageDTO en Message
+  // Méthode pour transformer un MessageDTO en une entité Message
   public Message toEntity(MessageDTO messageDTO) {
-    // Récupération de l'utilisateur et de la location à partir des IDs
+    // Recherche de l'utilisateur à partir de son ID
     AppUser user = userRepository.findById(messageDTO.getUser_id())
-        .orElseThrow(() -> new IllegalArgumentException("Invalid User ID"));
+            .orElseThrow(() -> new IllegalArgumentException("Utilisateur introuvable"));
 
+    // Recherche de la location à partir de son ID
     Rental rental = rentalRepository.findById(messageDTO.getRental_id())
-        .orElseThrow(() -> new IllegalArgumentException("Invalid Rental ID"));
+            .orElseThrow(() -> new IllegalArgumentException("Location introuvable"));
 
-    // Création de l'entité Message
+    // Création d'une nouvelle instance de Message et attribution des valeurs
     Message message = new Message();
-    message.setId(messageDTO.getId()); // On ajoute l'ID du Message
-    message.setUser(user); // Utilisation de l'objet AppUser
-    message.setRental(rental); // Utilisation de l'objet Rental
-    message.setMessage(messageDTO.getMessage());
+    message.setId(messageDTO.getId()); // On récupère l'ID du Message
+    message.setUser(user); // Association de l'utilisateur à ce message
+    message.setRental(rental); // Association de la location à ce message
+    message.setMessage(messageDTO.getMessage()); // Le contenu du message
 
     return message;
   }
 
-  // Convertir un Message en MessageDTO
+  // Méthode pour transformer une entité Message en MessageDTO
   public MessageDTO toDTO(Message message) {
+    // Création d'un objet MessageDTO vide
     MessageDTO dto = new MessageDTO();
-    dto.setId(message.getId());
-    dto.setRental_id(message.getRental().getId());
-    dto.setUser_id(message.getUser().getId());
-    dto.setMessage(message.getMessage());
+    dto.setId(message.getId()); // Récupération de l'ID du Message
+    dto.setRental_id(message.getRental().getId()); // Récupération de l'ID de la location
+    dto.setUser_id(message.getUser().getId()); // Récupération de l'ID de l'utilisateur
+    dto.setMessage(message.getMessage()); // Le contenu du message
+
     return dto;
   }
 }
