7a4092609fdb036bff0370281b557892e5600078
chambrin <achambrin@outlook.fr>
2025-02-03 08:59:20 +0100

    🎨 exception

---
+++ src/main/java/com/openclassroms/ApiP3/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.openclassroms.ApiP3.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+}
